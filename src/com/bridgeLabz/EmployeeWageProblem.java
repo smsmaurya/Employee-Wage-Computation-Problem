0f4bc1b3811def10a96139417c4aeb8405dbd82f
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-03 11:14:58 +0530

    Added :- Calculate employee wage for multiple comapny

--- src/com/bridgeLabz/EmployeeWageProblem.java
+++ src/com/bridgeLabz/EmployeeWageProblem.java
@@ -5,16 +5,13 @@ public class EmployeeWageProblem {
     // Constant instance variable
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 150;
 
-    public static int computeEmpWage(){
+    public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxPerHourPerMonth){
         // Local variables
         int empHrs, totalEmpHrs = 0, totalWorkingDays=0;
 
-        // Logic of employee wage computation number of working days
-        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS){
+        // Logic of employee wage computation for multi company
+        while (totalEmpHrs <= maxPerHourPerMonth && totalWorkingDays < numOfWorkingDays){
             totalWorkingDays++;
             int empCheck = (int) (Math.floor(Math.random()*10)%3);
             switch (empCheck){
@@ -30,13 +27,14 @@ public class EmployeeWageProblem {
             totalEmpHrs += empHrs;
             System.out.println("Days : "+totalWorkingDays+" Emp hrs : "+empHrs );
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage of "+totalEmpHrs+" hrs : "+totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Emp Wage for company : "+company+" is : "+totalEmpWage);
         return totalEmpWage;
     }
 
     public static void main(String[] args) {
-        computeEmpWage();
 
+        computeEmpWage("DMart",20, 2, 100);
+        computeEmpWage("TATA", 30, 4, 80);
     }
 }
