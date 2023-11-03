4d9f788b68794cf094ea1b3d9ae4360d975ecc05
smsmaurya <sumeshmaurya786@gmail.com>
2023-11-03 05:50:55 +0530

    Added :- Add Part Time Employee and Wage

--- src/com/bridgeLabz/EmployeeWageProblem.java
+++ src/com/bridgeLabz/EmployeeWageProblem.java
@@ -2,23 +2,26 @@ package com.bridgeLabz;
 
 public class EmployeeWageProblem {
     public static void main(String[] args) {
-        // constants
-        int IS_FULL_TIME = 1;
+        //constants
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
 
-        // variables
+        //variables
         int empHrs;
         int empWage;
 
         //computation
-        double empCheck = Math.floor(Math.random()*10)%2;
-        if(empCheck==IS_FULL_TIME){
+        double empCheck = Math.floor(Math.random()*10)%3;
+        if(empCheck == IS_PART_TIME){
+            empHrs = 4;
+        } else if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
-        }
-        else{
+        } else {
             empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Employee Wage : "+ empWage);
+        System.out.println("Emp wage : "+empWage);
+
     }
 }
